6db428ee2c47cbc2b96443f9b87ddbd3e315509c
Marco Leonardini <marco_leonardini@hotmail.com>
2020-02-28 11:24:41 -0400

    Added cards and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
       home: HomePage(),
     );
@@ -19,6 +20,88 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.teal,
+          padding: EdgeInsets.all(24.0),
+          constraints: BoxConstraints.expand(),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(80.0),
+                    child: Container(
+                      constraints:
+                          BoxConstraints.expand(width: 160.0, height: 160.0),
+                      child: Image.network(
+                          'https://avatars3.githubusercontent.com/u/2449572?s=400&u=e54a9086489ee6bdacb6345d255e64093b71bd1b&v=4'),
+                    )),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  'Marco Leonardini',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    color: Colors.white,
+                    fontFamily: 'oxanium',
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  'Flutter Dev',
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 3,
+                  child: Divider(
+                    color: Colors.white,
+                    thickness: 1,
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+591-72540680',
+                      style: TextStyle(
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'marco.ruben.leonardini\n@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
